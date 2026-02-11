1e669d973f6a5e23d08db9200b22ab986d55c7f1
adityayadav176 <ay6598aditya@gmail.com>
2026-02-11 14:57:21 +0530

    Add TaskItem.js and TaskItems Best Ui

--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -4,8 +4,31 @@ import './Css/TaskItem.css'
 function TaskItem() {
   return (
     <>
-     
-      
+      <div className="Task-container">
+        <div className="task-data">
+        <div className="task-title">
+            <h2>Complete project documentation</h2>
+            <p className='Priority'> high</p>
+        </div>
+        <div className="task-btn">
+            <i class="fa-solid fa-check task-tick-btn"></i>
+            <i class="fa-regular fa-trash-can task-trash-btn"></i>
+            </div>
+        </div>
+        <div className="Task-description">
+            <p>Write comprehensive documentation for the new features</p>
+        </div>
+        <div className="task-process-date">
+           <div className="process">
+            <i class="fa-solid fa-check"></i>
+            Complete
+           </div>
+           <div className="task-date">
+            <i class="fa-regular fa-clock"></i>
+            03/01/2025 
+           </div>
+        </div>
+      </div> 
     </>
   )
 }
